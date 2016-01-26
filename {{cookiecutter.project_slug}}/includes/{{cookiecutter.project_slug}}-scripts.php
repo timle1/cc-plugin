e56bebc21d5c0ef7fcec501280048627e2074cbc
The One <theonerightoption@gmail.com>
2016-01-26 16:09:25 -0500

    prefix / to includes/~script.php

--- {{cookiecutter.project_slug}}/includes/{{cookiecutter.project_slug}}-scripts.php
+++ {{cookiecutter.project_slug}}/includes/{{cookiecutter.project_slug}}-scripts.php
@@ -2,8 +2,8 @@
 
 // Add Scripts
 function {{ cookiecutter.project_namespace }}_add_scripts() {
-    wp_enqueue_style('{{ cookiecutter.project_namespace }}-main-style', plugins_url() . '{{cookiecutter.project_slug}}/css/style.css');
-    wp_enqueue_script('{{ cookiecutter.project_namespace }}-main-style', plugins_url() . '{{cookiecutter.project_slug}}/js/main.js');
+    wp_enqueue_style('{{ cookiecutter.project_namespace }}-main-style', plugins_url() . '/{{cookiecutter.project_slug}}/css/style.css');
+    wp_enqueue_script('{{ cookiecutter.project_namespace }}-main-style', plugins_url() . '/{{cookiecutter.project_slug}}/js/main.js');
 }
 
 add_action('wp_enqueue_scripts', '{{ cookiecutter.project_namespace }}_add_scripts');
