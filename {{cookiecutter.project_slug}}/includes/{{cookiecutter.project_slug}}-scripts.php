60f74404d9b0d9c83aabfbf13b4a83eff7147133
The One <theonerightoption@gmail.com>
2016-02-05 16:22:48 -0500

    add jquery to main script

--- {{cookiecutter.project_slug}}/includes/{{cookiecutter.project_slug}}-scripts.php
+++ {{cookiecutter.project_slug}}/includes/{{cookiecutter.project_slug}}-scripts.php
@@ -3,7 +3,7 @@
 // Add Scripts
 function {{ cookiecutter.project_namespace }}_add_static() {
     wp_enqueue_style('{{ cookiecutter.project_namespace }}-main-style', plugins_url() . '/{{cookiecutter.project_slug}}/css/style.css');
-    wp_enqueue_script('{{ cookiecutter.project_namespace }}-main-script', plugins_url() . '/{{cookiecutter.project_slug}}/js/main.js');
+    wp_enqueue_script('{{ cookiecutter.project_namespace }}-main-script', plugins_url() . '/{{cookiecutter.project_slug}}/js/main.js', array('jquery'));
 }
 
 add_action('wp_enqueue_scripts', '{{ cookiecutter.project_namespace }}_add_static');
